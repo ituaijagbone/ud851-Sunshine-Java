77d919604ea6f6e0ef1193eb23db10fe82c0fed9
itua <itua.ijagbone@morningstar.com>
2018-02-04 02:32:39 -0600

    completed S04.03

--- S04.03-Exercise-AddMapAndSharing/app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ S04.03-Exercise-AddMapAndSharing/app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -2,7 +2,11 @@ package com.example.android.sunshine;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
@@ -31,4 +35,21 @@ public class DetailActivity extends AppCompatActivity {
 
     // TODO (3) Create a menu with an item with id of action_share
     // TODO (4) Display the menu and implement the forecast sharing functionality
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.forecast_share, menu);
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+        menuItem.setIntent(getForecastIntent());
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    private Intent getForecastIntent() {
+        String mimeType = "text/plain";
+        return ShareCompat.IntentBuilder.from(this)
+                .setType(mimeType)
+                .setText(mForecast + FORECAST_SHARE_HASHTAG)
+                .getIntent();
+    }
 }
--- S04.03-Exercise-AddMapAndSharing/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S04.03-Exercise-AddMapAndSharing/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -23,7 +23,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -222,7 +221,24 @@ public class MainActivity extends AppCompatActivity implements ForecastAdapterOn
         }
 
         // TODO (2) Launch the map when the map menu item is clicked
-
+        if (id == R.id.action_open_map) {
+            openMap();
+            return true;
+        }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void openMap() {
+        String location = SunshinePreferences.getPreferredWeatherLocation(this);
+        Uri.Builder uriBuilder = new Uri.Builder();
+        Uri address = uriBuilder.scheme("geo")
+                .path("0,0")
+                .query(location)
+                .build();
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(address);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 }
