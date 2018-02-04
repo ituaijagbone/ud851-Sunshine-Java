9c8e6e3d9cc1aea6ed7fb624564eff3400570bd1
itua <itua.ijagbone@morningstar.com>
2018-02-04 01:56:29 -0600

    completed S04.02

--- S04.02-Exercise-DisplayDayForecast/app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ S04.02-Exercise-DisplayDayForecast/app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -1,7 +1,9 @@
 package com.example.android.sunshine;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -13,5 +15,10 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
 
         // TODO (2) Display the weather forecast that was passed from MainActivity
+        Intent intent = getIntent();
+        if (intent.hasExtra(Intent.EXTRA_TEXT)) {
+            TextView textView = (TextView) findViewById(R.id.tv_display_selected_weather);
+            textView.setText(intent.getStringExtra(Intent.EXTRA_TEXT));
+        }
     }
 }
